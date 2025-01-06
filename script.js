71edae9ea83d12572ffd7888f750cef49367c548
Rudra Sharma <168713568+Rudrasharma214@users.noreply.github.com>
2025-01-06 23:18:44 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Let's write JavaScript")
 
-async function getsongs(){
+async function getsongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text()
     console.log(response)
@@ -12,8 +12,8 @@ async function getsongs(){
     let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
-            songs.push(element.href)
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1])
         }
     }
 
@@ -23,8 +23,27 @@ async function main() {
     let songs = await getsongs()
     console.log(songs)
 
-    var audio = new Audio(songs[0])
+    let songul = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songul.innerHTML = songul.innerHTML + `<li>
+                            <img class="invert" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20"," ")} </div>
+                                <div>Rudra</div>
+                            </div>
+                            <span class="p1" >Play Now</span>
+                            <img class="invert" src="img/play.svg" alt="">
+                        </li>`
+    }
+
+    var audio = new Audio(songs[2])
     // audio.play()
+
+    audio.addEventListener("loadeddata", () => {
+        let duration = audio.duration; //hold the duration of song in seconds
+        console.log(duration)
+
+    })
 }
 
 main()
